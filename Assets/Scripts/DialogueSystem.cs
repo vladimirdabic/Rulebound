6a6c1fdb083aac8bd863352320a8e094185aacf0
vladimirdabic <vddabic@gmail.com>
2025-08-08 01:05:09 +0200

    Add choice branching

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -13,12 +14,18 @@ public class DialogueSystem : MonoBehaviour
 {
     public static DialogueSystem Instance;
 
-    [Header("UI References")]
+    [Header("Dialog References")]
     public GameObject DialoguePanel;
     public TMP_Text MessageText;
     public Image PortraitImage;
-    public Transform ChoicesParent;
-    public GameObject ChoiceButtonPrefab;
+
+    [Header("Choice References")]
+    public GameObject ChoicePanel;
+    public TMP_Text ChoiceText;
+    public TMP_Text Choices;
+    public Image Selector;
+    [SerializeField] private float _selectorOffset;
+    [SerializeField] private float _selectorItemJump;
 
     [Header("Input References")]
     [SerializeField] private PlayerInput _playerInput;
@@ -31,10 +38,18 @@ public class DialogueSystem : MonoBehaviour
     private IEnumerator _lineEnumerator;
     private DialogueLine _currentLine;
     private Dialogue _currentDialog;
-    
+    private int _choiceIdx;
     
     private InputAction _advanceAction;
-    private bool _finished;
+    private InputAction _upAction;
+    private InputAction _downAction;
+    //private bool _finished;
+    private State _state;
+
+    private enum State
+    {
+        WAITING, WRITING, CHOICE
+    }
 
     private void Awake()
     {
@@ -44,16 +59,22 @@ public class DialogueSystem : MonoBehaviour
 
         InputActionMap map = _playerInput.actions.FindActionMap("Dialogue");
         _advanceAction = map.FindAction("Advance");
+        _upAction = map.FindAction("Up");
+        _downAction = map.FindAction("Down");
     }
 
     private void OnEnable()
     {
         _advanceAction.performed += _advanceAction_performed;
+        _upAction.performed += _choiceActionPeformed;
+        _downAction.performed += _choiceActionPeformed;
     }
 
     private void OnDisable()
     {
         _advanceAction.performed -= _advanceAction_performed;
+        _upAction.performed -= _choiceActionPeformed;
+        _downAction.performed -= _choiceActionPeformed;
     }
 
     public void PlayDialogue(Dialogue diag)
@@ -78,13 +99,26 @@ public class DialogueSystem : MonoBehaviour
             yield return new WaitForSeconds(line.SecondsBefore);
         }
 
-        DialoguePanel.SetActive(true);
         _currentLine = line;
+
+        if (line.IsBranching)
+        {
+            ChoiceText.text = line.Message;
+            Choices.text = string.Join('\n', line.Choices.Select(i => i.Choice));
+            _choiceIdx = 0;
+            ChoicePanel.SetActive(true);
+            MoveSelectorToChoice();
+            _state = State.CHOICE;
+            yield break;
+        }
+
+        DialoguePanel.SetActive(true);
         PortraitImage.sprite = line.Portrait != null ? line.Portrait : _currentDialog.MainPortrait;
-        _finished = false;
+        //_finished = false;
+        _state = State.WRITING;
 
         // Magic numbers, too lazy to turn them into constants...
-        if(PortraitImage.sprite == null)
+        if (PortraitImage.sprite == null)
         {
             MessageText.rectTransform.anchoredPosition = new Vector2(19f, -12.1f);
             MessageText.rectTransform.sizeDelta = new Vector2(540.28f, 112.8449f);
@@ -97,9 +131,6 @@ public class DialogueSystem : MonoBehaviour
             PortraitImage.gameObject.SetActive(true);
         }
 
-        // TODO: Branching paths
-        //if (line.IsBranching) {}
-
         OnLineStarted?.Invoke(line);
         MessageText.text = string.Empty;
 
@@ -109,7 +140,8 @@ public class DialogueSystem : MonoBehaviour
             yield return new WaitForSeconds(0.03f);
         }
 
-        _finished = true;
+        //_finished = true;
+        _state = State.WAITING;
     }
 
     private void PlayNextLine()
@@ -127,16 +159,66 @@ public class DialogueSystem : MonoBehaviour
 
     private void _advanceAction_performed(InputAction.CallbackContext obj)
     {
-        if (!_finished) return;
+        //if (!_finished) return;
 
-        OnLineEnded?.Invoke(_currentLine);
-        PlayNextLine();
+        switch(_state)
+        {
+            case State.WAITING:
+                OnLineEnded?.Invoke(_currentLine);
+                PlayNextLine();
+                break;
+
+            case State.CHOICE:
+                DialogueChoice choice = _currentLine.Choices[_choiceIdx];
+
+                if(choice.NextDialogue == null)
+                {
+                    EndDialogue();
+                    return;
+                }
+
+                PlayDialogue(choice.NextDialogue);
+                ChoicePanel.SetActive(false);
+                break;
+
+            default:
+                return;
+        }
+
     }
 
-    public void EndDialogue()
+    private void _choiceActionPeformed(InputAction.CallbackContext obj)
+    {
+        switch(obj.action.name)
+        {
+            case "Up":
+                _choiceIdx = Mathf.Max(--_choiceIdx, 0);
+                break;
+
+            case "Down":
+                _choiceIdx = Mathf.Min(++_choiceIdx, _currentLine.Choices.Length - 1);
+                break;
+        }
+
+        MoveSelectorToChoice();
+    }
+
+    private void EndDialogue()
     {
         DialoguePanel.SetActive(false);
+        ChoicePanel.SetActive(false);
         _playerInput.SwitchCurrentActionMap("Player");
         OnDialogueEnded?.Invoke(_currentDialog);
+    }
+
+    // Taken from InventorySystem
+    private void MoveSelector(Vector3 position)
+    {
+        Selector.transform.position = position - new Vector3(_selectorOffset, 14.8f);
+    }
+
+    private void MoveSelectorToChoice()
+    {
+        MoveSelector(Choices.transform.position - new Vector3(0, _selectorItemJump * _choiceIdx));
     }
 }
